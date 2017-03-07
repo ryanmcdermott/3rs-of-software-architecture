368b9cfaf87741afba7d5833876b67209d9def99
Ryan McDermott <ryantmcdermott@gmail.com>
2017-03-06 21:48:20 -0800

    Fix Carts

--- src/3-refactorable/good/Cart.js
+++ src/3-refactorable/good/Cart.js
@@ -4,20 +4,10 @@ export default class Cart extends Component {
   constructor(props) {
     super();
     this.state = {
-      cart: window.cart,
       localCurrency: props.localCurrency,
       inventory: props.inventory,
     };
 
-    // Repeatedly sync local cart to global cart. Bad!
-    window.setInterval(
-      () => {
-        this.setState({
-          cart: window.cart,
-        });
-      },
-      1000,
-    );
     this.CurrencyConverter = props.currencyConverter;
   }
 
@@ -25,7 +15,7 @@ export default class Cart extends Component {
     return (
       <div>
         <h2>Cart</h2>
-        {this.state.cart.length === 0
+        {window.cart.length === 0
           ? <p>Nothing in the cart</p>
           : <table style={{ width: '100%' }}>
             <tbody>
@@ -38,7 +28,7 @@ export default class Cart extends Component {
                     Price
                   </th>
               </tr>
-              {this.state.cart.map((itemId, idx) => (
+              {window.cart.map((itemId, idx) => (
                 <tr key={idx}>
                   <td>
                     {this.state.inventory[itemId].product}
