f91526351852669aea1394b5721c605b6533fd8c
Ryan McDermott <ryantmcdermott@gmail.com>
2017-03-07 21:58:01 -0800

    Fix more Good Refactorable cart

--- src/3-refactorable/good/Cart.js
+++ src/3-refactorable/good/Cart.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 export default class Cart extends Component {
   constructor(props) {
     super();
-    this.state = {
-      localCurrency: props.localCurrency,
-      inventory: props.inventory,
-    };
 
     this.CurrencyConverter = props.currencyConverter;
   }
@@ -15,7 +11,7 @@ export default class Cart extends Component {
     return (
       <div>
         <h2>Cart</h2>
-        {window.cart.length === 0
+        {this.state.cart.length === 0
           ? <p>Nothing in the cart</p>
           : <table style={{ width: '100%' }}>
             <tbody>
@@ -28,7 +24,7 @@ export default class Cart extends Component {
                     Price
                   </th>
               </tr>
-              {window.cart.map((itemId, idx) => (
+              {this.state.cart.map((itemId, idx) => (
                 <tr key={idx}>
                   <td>
                     {this.state.inventory[itemId].product}
